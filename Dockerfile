c280c15382d19d1a2e9784c1fc0ccf68e3b3f2f0
Marcus <minh.nguyen@triona.fi>
2024-03-23 23:58:47 +0200

    feat: initial

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:18.0.0
+
+WORKDIR /app
+COPY package.json ./
+
+RUN npm install
+COPY . .
+EXPOSE 8000
+CMD ["npm", "run", "start"]
---
+++ server.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import express, { Application, Request, Response } from "express";
+
+//For env File
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Express & TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
